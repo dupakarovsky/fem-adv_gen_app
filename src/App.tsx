75795c5b30e995b3c6407ba99291bca2c0454b7b
gustavo <dupakarovsky@outlook.com>
2024-04-08 14:18:32 -0300

    struggling animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
+import { useCallback, useEffect, useState } from "react";
 
 /*
     ================
@@ -8,34 +9,120 @@ import { motion } from "framer-motion";
 const App: React.FC = () => {
     return (
         <>
-            <AdviceGenerator />
+            <AdviceGenerator>
+                <Advice>
+                    <AdviceNumber></AdviceNumber>
+                    <AdviceBox></AdviceBox>
+                </Advice>
+                <Picture
+                    sources={[
+                        { sourceSet: "pattern-divider-mobile.svg", media: "(max-width: 375px)" },
+                        { sourceSet: "pattern-divider-desktop.svg", media: "(min-width: 376px)" },
+                    ]}
+                    fallbackImgURL="pattern-divider-desktop.svg"
+                />
+                <Button></Button>
+            </AdviceGenerator>
         </>
     );
 };
-
 export default App;
+
+/*
+    ==================
+    * HOOK USE ADVICE   
+    ==================
+*/
+type APIJSONResponseType = {
+    slip: {
+        id: number;
+        advice: string;
+    };
+};
+function useAdvice() {
+    const [adviceSlip, setAdviceSlip] = useState<APIJSONResponseType | null>(null);
+    const [errorMessage, setErrorMessage] = useState<Error | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const fetchAdvice = useCallback((url: string) => {
+        setIsLoading(true);
+
+        fetch(url)
+            .then((result: Response) => {
+                // console.log(result);
+                if (!result.ok) throw new Error("The API has encontered and error");
+                return result.json();
+            })
+            .then((jsonResponse: APIJSONResponseType) => {
+                // console.log({ "slip": jsonResponse.slip, "id": jsonResponse.slip.id, "advice": jsonResponse.slip.advice });
+                setAdviceSlip(jsonResponse);
+            })
+            .catch((err: unknown) => {
+                switch (true) {
+                    case err instanceof Error: {
+                        // console.error(err.message);
+                        setErrorMessage(err);
+                        break;
+                    }
+                    default: {
+                        // console.log(err);
+                        const unknowError: Error = new Error("Something wrong has happened, which is a mistery.");
+                        setErrorMessage(unknowError);
+                        throw unknowError;
+                    }
+                }
+            })
+            .finally(() => {
+                console.log("fetchAdvice completed");
+                setIsLoading(false);
+            });
+    }, []);
+
+    useEffect(() => {
+        setErrorMessage(null);
+        fetchAdvice("https://api.adviceslip.com/advice");
+    }, [fetchAdvice]);
+
+    console.log({ adviceSlip, errorMessage, isLoading });
+
+    return {
+        adviceSlip,
+        setAdviceSlip,
+        isLoading,
+        setIsLoading,
+        errorMessage,
+        setErrorMessage,
+    };
+}
 
 /*
     =====================
     * ADVICE GENERATOR
     =====================
 */
-const AdviceGenerator: React.FC = () => {
+type AdviceGeneratorType = {
+    children?: React.ReactNode;
+};
+const AdviceGenerator: React.FC<AdviceGeneratorType> = ({ children }) => {
+    const { isLoading } = useAdvice();
+
+    const duration: number = 0.2;
+    const delay: number = 0.2;
+
+    const adviceGeneratorVariants: Variants = {
+        start: { opacity: 0, y: "10%" },
+        finish: { opacity: 1, y: "-10%", transition: { duration: duration, ease: "linear", delay: delay } },
+    };
+
     return (
-        <div className="advice-generator">
-            <Advice>
-                <AdviceNumber></AdviceNumber>
-                <AdviceBox advice="It is easy to sit up and take notice, what's difficult is getting up and takeing action."></AdviceBox>
-            </Advice>
-            <Picture
-                sources={[
-                    { sourceSet: "pattern-divider-mobile.svg", media: "(max-width: 375px)" },
-                    { sourceSet: "pattern-divider-desktop.svg", media: "(min-width: 376px)" },
-                ]}
-                fallbackImgURL="pattern-divider-desktop.svg"
-            />
-            <Button></Button>
-        </div>
+        <motion.div
+            className="advice-generator"
+            animate={isLoading ? "start" : "finish"}
+            variants={adviceGeneratorVariants}
+            onAnimationComplete={(definition) => console.log("Animation has completed", definition)}
+        >
+            {children}
+        </motion.div>
     );
 };
 
@@ -57,7 +144,22 @@ const Advice: React.FC<AdviceType> = ({ children }) => {
     =====================
 */
 const AdviceNumber: React.FC = () => {
-    return <span className="advice-number">advice # 117</span>;
+    const { adviceSlip, isLoading } = useAdvice();
+
+    const duration: number = 0.2;
+    const delay: number = 0.2;
+
+    const adviceNumberVariants: Variants = {
+        start: { opacity: 0, scale: 2, filter: "blur(2px)" },
+        finish: { opacity: 1, scale: 1, filter: " blur(0px)", transition: { duration: duration, delay: delay } },
+    };
+
+    return (
+        <motion.span className="advice-number" variants={adviceNumberVariants} animate={isLoading ? "start" : "finish"}>
+            {isLoading && `advice # 000`}
+            {adviceSlip?.slip && `advice # ${adviceSlip.slip.id}`}
+        </motion.span>
+    );
 };
 
 /*
@@ -65,15 +167,30 @@ const AdviceNumber: React.FC = () => {
     * ADVICE BOX
     =====================
 */
-type AdviceBoxType = {
-    advice: string;
-};
-const AdviceBox: React.FC<AdviceBoxType> = ({ advice }) => {
-    const stagger: number = 0.025;
-    const dur: number = 0.02;
 
-    const adviceArray = advice.split("");
-    const renderedAdvice = adviceArray.map((char: string, idx: number) => {
+const AdviceBox: React.FC = () => {
+    const { isLoading, adviceSlip, errorMessage } = useAdvice();
+
+    const stagger: number = 0.025;
+    const duration: number = 0.2;
+    const delay: number = 0.4;
+
+    const adviceBoxVariants: Variants = {
+        start: { opacity: 0 },
+        finish: (custom: number) => {
+            return { opacity: 1, transition: { delay: delay + stagger * custom, duration: duration } };
+        },
+    };
+
+    const displayOutput = () => {
+        if (adviceSlip?.slip) return adviceSlip.slip.advice.split("");
+        if (errorMessage && errorMessage !== null) return errorMessage.message.split("");
+        return [];
+    };
+
+    const adviceCharsArray: string[] = displayOutput();
+
+    const renderedAdvice = adviceCharsArray.map((char: string, idx: number) => {
         if (char === " ") {
             char = "\u0020";
         }
@@ -81,9 +198,9 @@ const AdviceBox: React.FC<AdviceBoxType> = ({ advice }) => {
             <motion.span
                 key={idx + 1}
                 className={`char--${idx + 1}`}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.4 + stagger * idx, duration: dur }}
+                variants={adviceBoxVariants}
+                animate={isLoading ? "start" : "finish"}
+                custom={idx}
             >
                 {char}
             </motion.span>
@@ -95,18 +212,18 @@ const AdviceBox: React.FC<AdviceBoxType> = ({ advice }) => {
             <div className="advice">
                 <motion.span
                     className={`left-quote char--${0}`}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: 0.4 + stagger * 0, duration: dur }}
+                    variants={adviceBoxVariants}
+                    animate={isLoading ? "start" : "finish"}
+                    custom={0}
                 >
                     {"\u201C"}
                 </motion.span>
                 {renderedAdvice}
                 <motion.span
-                    className={`right-quote char--${adviceArray.length}`}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: 0.4 + stagger * adviceArray.length, duration: dur }}
+                    className={`right-quote char--${adviceCharsArray.length}`}
+                    variants={adviceBoxVariants}
+                    animate={isLoading ? "start" : "finish"}
+                    custom={adviceCharsArray.length}
                 >
                     {"\u201D"}
                 </motion.span>
@@ -142,6 +259,11 @@ const Picture: React.FC<PictureType> = ({ sources, fallbackImgURL }) => {
     );
 };
 
+/*
+    =====================
+    * BUTTON
+    =====================
+*/
 const Button: React.FC = () => {
     return (
         <button className="button">
